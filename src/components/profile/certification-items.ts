dcdb8f8cf2f58d80cb401f4f769d7597f637df89
khokao <kokihokao@gmail.com>
2025-05-31 16:24:27 +0900

    add url attr to certification item

--- src/components/profile/certification-items.ts
+++ src/components/profile/certification-items.ts
@@ -1,11 +1,18 @@
 export interface CertificationItem {
   title: string;
   date: Date;
+  url: string | null;
 }
 
 export const certificationItems: CertificationItem[] = [
   {
     title: "TOEIC : 970 / 990",
     date: new Date("2021-04-01"),
+    url: null,
+  },
+  {
+    title: "MITOU Super Creator",
+    date: new Date("2025-05-29"),
+    url: "https://www.ipa.go.jp/jinzai/mitou/koubo/career/2024/2024-supercreator-04.html",
   },
 ];
